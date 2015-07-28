cbc6d70e6c8b8325be401600275a4caf64265b0b
Saurabh <>
2015-07-28 15:34:41 +0530

    This is Test

--- MainActivity.cs
+++ MainActivity.cs
@@ -24,7 +24,7 @@ namespace dsfds
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
+			//Test Apple
 			button.Click += delegate {
 				button.Text = string.Format ("{0} clicks!", count++);
 			};
